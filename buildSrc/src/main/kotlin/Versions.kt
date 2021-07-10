68be0e6c6cfbb692ec124c559b974e269980de31
jiangdashao <qq1609403959@outlook.com>
2021-07-10 13:44:40 +0800

    style: 添加buildSrc变量注释

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,4 +1,12 @@
-val hiltVersion = "2.37"
-val composeVersion = "1.0.0-rc01"
-val pagingVersion = "3.0.0"
-val accVersion = "0.13.0"
+// JetPack Compose 版本
+const val composeVersion = "1.0.0-rc01"
+
+// Hilt 版本
+const val hiltVersion = "2.37"
+
+// Paging 版本
+const val pagingVersion = "3.0.0"
+
+// accompanist 版本
+// https://github.com/google/accompanist
+const val accVersion = "0.13.0"
